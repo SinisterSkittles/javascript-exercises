5b926c863e01e7ed4bfc777c44b6559ce1ef5739
fatin <abdelrahimfa@gmail.com>
2025-08-24 13:46:17 -0500

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (typeof num1 != 'number' || 
+        typeof num2 != 'number' || 
+        num1 < 0 || num2 < 0 || 
+        num1 % 1 !== 0 || num2 % 1 !== 0) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        for (num2; num2 <= num1; num2++) {
+            sum += num2;
+        }
+    } else {
+        for (num1; num1 <= num2; num1++) {
+            sum += num1;
+        }
+    }
+
+    return sum;
 };
+
+// 1, 4
+// 1 + 2 + 3 + 4
+// 1, 1
+// 
 
 // Do not edit below this line
 module.exports = sumAll;
